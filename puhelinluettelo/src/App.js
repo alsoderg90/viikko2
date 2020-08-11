73b8a1200b71cb37571a85003f0ad14b6e23341b
alexander <alsoderg@student.jyu.fi>
2020-08-11 09:15:07 +0300

    2.9 part4

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
+
   const [ persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
     { name: 'Ada Lovelace', number: '39-44-5323523' },
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, showFiltered ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -20,7 +23,7 @@ const App = () => {
     if (persons.map(person => person.name).includes(newName) === false) {
       setPersons(persons.concat(person))
       setNewName('')
-
+      setNewNumber('')
     } 
     else {
       window.alert(`${newName} is already added to phonebook`)
@@ -35,10 +38,28 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+  const handleFilter = (event) => {
+    showFiltered(event.target.value)
+  }
+
+  const Persons = (props) => {
+    
+    const copy = props.persons.filter(person =>
+    person.name.toLowerCase().includes(newFilter.toLowerCase()))
+    return (
+      copy.map(a => <p key={a.name}>{a.name} {a.number}</p>)
+    )
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={newFilter} 
+        onChange={handleFilter}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} 
@@ -53,7 +74,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <Persons persons={persons}/>
     </div>
   )
 
