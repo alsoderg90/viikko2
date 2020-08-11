c266e702ff4387ae4feab7b1e684195016044c8a
alexander <alsoderg@student.jyu.fi>
2020-08-11 13:28:24 +0300

    2.11 puhelunluettelo step6

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => (
   <div>
@@ -33,12 +34,7 @@ const Persons = (props) => {
 
 const App = () => {
 
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, showFiltered ] = useState('')
@@ -72,6 +68,17 @@ const App = () => {
     showFiltered(event.target.value)
   }
 
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
+  console.log('render',persons.length, 'notes')
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -86,3 +93,4 @@ const App = () => {
 }
 
 export default App
+
