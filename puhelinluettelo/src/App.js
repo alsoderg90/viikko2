2006328a9802c439d26b759e5c19d85bac71336d
alexander <alsoderg@student.jyu.fi>
2020-08-11 10:17:58 +0300

    2.10 step5

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -1,5 +1,36 @@
 import React, { useState } from 'react'
 
+const Filter = (props) => (
+  <div>
+  filter shown with: <input value={props.value} 
+  onChange={props.onChange}/>
+  </div>
+)
+
+const Person = (props) => (
+  <form onSubmit={props.onSubmit}>
+    <div>
+    name: <input value={props.name} 
+    onChange={props.onChangeName}/>
+    </div>
+    <div>
+    number <input value={props.number} 
+    onChange={props.onChangeNumber}/>
+    </div>
+    <div>
+    <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+const Persons = (props) => {
+  const copy = props.persons.filter(person =>
+  person.name.toLowerCase().includes(props.filter.toLowerCase()))
+  return (
+    copy.map(a => <p key={a.name}>{a.name} {a.number}</p>)
+  )
+}
+
 const App = () => {
 
   const [ persons, setPersons] = useState([
@@ -8,7 +39,6 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newFilter, showFiltered ] = useState('')
@@ -19,8 +49,7 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-
-    if (persons.map(person => person.name).includes(newName) === false) {
+    if (persons.map(person => person.name.toLowerCase()).includes(newName.toLowerCase()) === false) {
       setPersons(persons.concat(person))
       setNewName('')
       setNewNumber('')
@@ -28,6 +57,7 @@ const App = () => {
     else {
       window.alert(`${newName} is already added to phonebook`)
     }
+
   }
 
   const handleName = (event) => {
@@ -42,42 +72,17 @@ const App = () => {
     showFiltered(event.target.value)
   }
 
-  const Persons = (props) => {
-    
-    const copy = props.persons.filter(person =>
-    person.name.toLowerCase().includes(newFilter.toLowerCase()))
-    return (
-      copy.map(a => <p key={a.name}>{a.name} {a.number}</p>)
-    )
-  }
-
-
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        filter shown with <input value={newFilter} 
-        onChange={handleFilter}/>
-      </div>
+      <Filter value={newFilter} onChange={handleFilter}/>
       <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input value={newName} 
-          onChange={handleName}/>
-        </div>
-        <div>
-          number <input value={newNumber} 
-          onChange={handleNumber}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Person onSubmit={addName} name={newName} onChangeName={handleName}
+      number={newNumber} onChangeNumber={handleNumber}/>
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={persons} filter={newFilter}/>
     </div>
   )
-
 }
 
 export default App
