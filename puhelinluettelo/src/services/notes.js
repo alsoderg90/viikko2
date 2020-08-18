598ca08fab266a99f4668708b0fe6d5c69506f41
alexander <alsoderg@student.jyu.fi>
2020-08-19 00:15:03 +0300

    korjaus tehtävään?

--- puhelinluettelo/src/services/notes.js
+++ puhelinluettelo/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/persons' /// 'http://localhost:3001/api/persons' <- vaihdettu tuosta
 
 const getAll = () => {
     return axios.get(baseUrl)
